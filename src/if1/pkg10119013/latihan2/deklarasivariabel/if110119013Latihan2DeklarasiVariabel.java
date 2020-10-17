08150c560c6ddbfcc0c71527e5c7f32f38b310e4
FionaAvila <fionaavila15@gmail.com>
2020-10-17 13:41:57 +0700

    Initial Commit

---
+++ src/if1/pkg10119013/latihan2/deklarasivariabel/if110119013Latihan2DeklarasiVariabel.java
@@ -0,0 +1,40 @@
+/**
+ * NAMA  : Fiona Avila Putri
+ * NIM   : 10119013
+ * KELAS : IF1 2019/2020 (PBO1)
+ */
+
+
+
+package if1.pkg10119013.latihan2.deklarasivariabel;
+
+/**
+ * 
+ * @author Fiona Avila
+ */
+
+public class if110119013Latihan2DeklarasiVariabel {
+
+    public static void main(String[] args) {
+        
+        //Deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14; //Konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel karakter = " + nilaiKarakter);
+        
+    }
+    
+}
